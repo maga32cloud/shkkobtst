6580653376a226afca1faf9bb68267a3502092b6
seunghwa <maga32@naver.com>
2025-09-21 17:17:05 +0900

    ERD추가

--- src/main/java/com/cafe/mobile/shcafe/common/jwt/JwtPassingURI.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.mobile.shcafe.common.jwt;
-
-public class JwtPassingURI {
-    public static final String[] PUBLIC_URIS = {
-            "/api/*/member",
-            "/api/*/member/login",
-            "/api/*/member/cancelWithdraw/*"
-    };
-}
--- src/main/java/com/cafe/mobile/shcafe/common/type/PayMtdCdConst.java
+++
@@ -1,9 +0,0 @@
-package com.cafe.mobile.shcafe.common.type;
-
-public class PayMtdCdConst {
-    public static final String KAKAO_PAY = "00";
-    public static final String CARD      = "10";
-    public static final String TRANSFER  = "20";
-
-    private PayMtdCdConst() {}
-}
